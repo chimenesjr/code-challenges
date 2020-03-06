2d3d3c31a5ae5a673762da02882a8ba8a7ebf766
chimenesjr <chimenesjr@gmail.com>
2020-03-06 07:03:16 +0000

    inicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace code_challenges
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new equilibrium();
+        }
+    }
+}
