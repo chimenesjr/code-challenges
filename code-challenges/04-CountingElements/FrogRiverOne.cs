a2928592f226373e0209db23bf6ee4e3f1538046
chimenesjr <chimenesjr@gmail.com>
2020-03-12 06:58:11 +0000

    frog

--- code-challenges/04-CountingElements/FrogRiverOne.cs
+++ code-challenges/04-CountingElements/FrogRiverOne.cs
@@ -16,9 +16,19 @@ namespace code_challenges
         public int find (int X, int[] A){
 
             var result = 0;
-
-            return result;
-
+			var max = 0;
+			
+			for (int i = 0; i < A.Length; i++)
+			{
+				Console.WriteLine($"{i} - {A[i]} - {max}");
+				if (A[i] > max)
+					max = A[i];
+			}
+			
+			if (max > X)
+				return -1;
+			
+            return (max+1);
         }
 
     }
